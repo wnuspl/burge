d3d1e822fd4ff03dcee32ba65910c619f8d43265
wnuspl <nusplwill@gmail.com>
2024-08-28 22:27:26 -0400

    Read me updated

--- burge-proj/target/debug/deps/burge_proj-dfd035153eabffac.d
+++ burge-proj/target/debug/deps/burge_proj-dfd035153eabffac.d
@@ -1,5 +1,5 @@
-C:\Users\nuspl\Documents\Obsidian\burge repo\burge-proj\target\debug\deps\libburge_proj-dfd035153eabffac.rmeta: src/main.rs
+C:\Users\nuspl\Documents\Projects\burge\v2\burge\burge-proj\target\debug\deps\libburge_proj-dfd035153eabffac.rmeta: src/main.rs
 
-C:\Users\nuspl\Documents\Obsidian\burge repo\burge-proj\target\debug\deps\burge_proj-dfd035153eabffac.d: src/main.rs
+C:\Users\nuspl\Documents\Projects\burge\v2\burge\burge-proj\target\debug\deps\burge_proj-dfd035153eabffac.d: src/main.rs
 
 src/main.rs:
